a2c45507e4500d3efbad5cc450b5432311161a8c
zhoriefzainulmuttaqin <zhoriefzainulmuttaqin@gmail.com>
2023-10-03 11:21:09 +0700

    fasilitas

--- lib/widget/breaking_news_card.dart
+++ lib/widget/breaking_news_card.dart
@@ -1,4 +1,4 @@
-import 'package:dispora_mobile_new/view/newberita.dart';
+// import 'package:dispora_mobile_new/view/newberita.dart';
 import 'package:dispora_mobile_new/widget/details_screen.dart';
 import 'package:dispora_mobile_new/widget/news_model.dart';
 import 'package:flutter/material.dart';
